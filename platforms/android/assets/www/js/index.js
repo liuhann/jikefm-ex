4833aad0ac2afa2b0b4cd598a04eadebd5f19200
liuhann <liuhann@gmail.com>
2015-05-26 23:32:00 +0800

    .

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -52,11 +52,12 @@ var app = {
 
 var myscroll = null;
 
+//$(ready);
 function ready() {
 	var size = $(window).width() / 18;
 	$("html").css("font-size", size);
 	myscroll = new IScroll("#file-list");
-	var tabscroll = new IScroll("#full-tab");
+	var tabscroll = new IScroll("#full-tab",{ scrollX: true, scrollY: false, snap: 'li'});
 	window.resolveLocalFileSystemURL(cordova.file.externalRootDirectory,
 		function(entries) {
 			openDir(entries);
